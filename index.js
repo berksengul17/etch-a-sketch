978a7eee1f411cd7cf8c7a1fc01e08392d270419
Berk Şengül <berksengul17@gmail.com>
2022-07-09 14:06:14 +0300

    Create the logic for creating dynamic grids

---
+++ index.js
@@ -0,0 +1,14 @@
+const container = document.getElementById("container");
+
+function createGrid(rows, cols){
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+
+    for(let i=0; i<(rows*cols); i++){
+        let cell = document.createElement("div");
+        cell.innerText = i + 1;
+        container.appendChild(cell).className = "grid-item";
+    }
+}
+
+createGrid(16, 16);

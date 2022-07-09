2895fae9331868edbb6bf9d05b7e812c93a69b9b
Berk Şengül <berksengul17@gmail.com>
2022-07-09 19:56:46 +0300

    Add click event listener

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const container = document.querySelector(".container");
 
+document.body.addEventListener("click", (e) => {
+    if(e.target.className == "grid-item"){
+        e.target.style.backgroundColor = "black";
+    } 
+    else{
+        console.log("Outside grid item");
+    }
+})
+
+
 function createGrid(rows, cols){
     container.style.setProperty("--grid-rows", rows);
     container.style.setProperty("--grid-cols", cols);
 
     for(let i=0; i<(rows*cols); i++){
         let cell = document.createElement("div");
-        // cell.innerText = i + 1;
         container.appendChild(cell).className = "grid-item";
     }
 }
